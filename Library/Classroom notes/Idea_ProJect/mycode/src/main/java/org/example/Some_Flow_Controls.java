3dc7d0fecd1d317b06bcf22e975d47c00ed755e0
罗洋 <786208586@qq.com>
2025-07-07 16:20:23 +0800

    Java学习笔记

---
+++ Library/Classroom notes/Idea_ProJect/mycode/src/main/java/org/example/Some_Flow_Controls.java
@@ -0,0 +1,84 @@
+package org.example;
+
+public class Some_Flow_Controls {
+    public static void main(String[] args) {
+//        for (int j = 1; j <= 5; j++) {
+//            for (int i = 0; i < 5; i++) {
+//                System.out.print(j + "\t");
+//            }
+//            System.out.println();
+//        }
+        //关于9*9乘法表的打印
+//        for (int i = 1; i <= 9; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(j+"×"+i+"="+(i*j<10?(" "+i*j):i*j)+" ");
+//            }
+//            System.out.println(" ");
+//        }
+        //嵌套打印图
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < 5; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println("");
+//        }
+//
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < 5; j++) {
+//                if (i%2!=0) {
+//                    if (j%2!=0) {
+//                        System.out.print("* ");
+//                    }else {
+//                        System.out.print("# ");
+//                    }
+//                }else {
+//                    if (j%2!=0) {
+//                        System.out.print("# ");
+//                    }else {
+//                        System.out.print("* ");
+//                    }
+//                }
+//            }
+//            System.out.println("");
+//        }
+//
+//        //产生100以内的随机数，直到随机数为88，终止
+//        int total = 0;
+//        while (true)    {
+//            int i = (int)(Math.random()*100);
+//            System.out.println(i);
+//            total ++;
+//            if(i==88){
+//                break;
+//            }
+//        }
+//        System.out.println("一共循环了:"+total+"次S");
+//        System.out.println("");
+//        //把100~150之间不能被3整除的数输出，并且每行输出5个
+//        int count = 0;
+//        for (int i = 100; i <150 ; i++) {
+//            if (i%3 ==0){
+//                continue;
+//            }else  {
+//                System.out.print(i+"\t");
+//                count++;
+//                if (count%5 == 0) {
+//                    System.out.println("");
+//                }
+//            }
+//        }
+//        //带标签的continue的语句
+//        //控制嵌套循环跳转打印101~150之间所有的质数
+//        outer:
+//        for (int i = 101; i < 150; i++) {
+//            for (int j = 2; j < i/2; j++) {
+//                if (i%j ==0){
+//                    continue outer;
+//                }
+//            }
+//            System.out.print(i+" ");
+//        }
+
+
+    }
+}
